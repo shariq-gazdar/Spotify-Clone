29823cefd8819cccdbd798fc84639b5435ff6be7
Shariq583 <gazdarshariq0@gmail.com>
2024-07-31 00:37:25 +0500

    react app add!

---
+++ spotify-clone-react/src/components/navBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function navBar() {
+  return <div id="navBar"></div>;
+}
+
+export default navBar;
---
+++ spotify-clone-react/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/components/navBar.{jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
