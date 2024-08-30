986a2eb1ca624b15aac062ee3f3960c2ebcd293a
= <gazdarshariq0@gmail.com>
2024-08-31 02:04:05 +0500

    things add!

--- spotify-clone-react/src/components/navBar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function navBar() {
-  return <div></div>;
-}
-
-export default navBar;
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="mainBody bg-zinc-900 w-screen h-screen"></div>;
+}
+
+export default App;
